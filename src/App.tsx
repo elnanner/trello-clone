bd32881f5cb8c7d331fdf1170279fd531ae017a7
Luciano Matsuo <matsuoluciano@gmail.com>
2021-07-23 14:11:04 -0300

    Adding Column and Card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Column } from './Column';
+import {Card} from './Card';
 import { AppContainer } from './styles';
 
 function App() {
   return (
     <AppContainer>
-      Columns will go here
+      <Column text="To Do">
+        <Card text="Generate app scaffold"></Card>
+        </Column>
+
+        <Column text="In Progress">
+        <Card text="Learn Typescript"></Card>
+        </Column>
+
+        <Column text="Done">
+        <Card text="Begin to use static typing"></Card>
+        </Column>
     </AppContainer>
   );
 }
--- src/styles.ts
+++ src/styles.ts
@@ -3,6 +3,7 @@
 
 import styled from 'styled-components';
 
+// div container
 export const AppContainer = styled.div`
     align-items: flex-start;
     background-color: #3179ba;
@@ -11,4 +12,31 @@ export const AppContainer = styled.div`
     height: 100%;
     padding: 20px;
     width: 100%;
+`
+// column
+export const ColumnContainer = styled.div`
+    background-color: #ebecf0;
+    width: 300px;
+    min-height: 40px;
+    margin-right: 20px;
+    border-radius: 3px;
+    padding: 8px 8px;
+    flex-grow: 0;
+    font-family: Arial;
+`
+// column title to wrap our column title
+export const ColumnTitle = styled.div`
+    padding: 6px 16px 12px;
+    font-weight: bold;
+`
+
+// styles for cards
+export const CardContainer = styled.div`
+    background-color: #fff;
+    cursor: pointer;
+    margin-bottom: 0.5rem;
+    padding: 0.5rem 1rem;
+    max-width: 300px;
+    border-radius: 3px;
+    box-shadows: #091e4249 0px 1px 0px 0px;
 `
